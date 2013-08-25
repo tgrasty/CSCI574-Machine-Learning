47faf1c9844008bb29b69151ed8f9b9cdae6ab50
Derek Harter <derek@harter.pro>
2013-08-25 16:02:27 -0500

    Added a run.py method, to make notebook startup easier.

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys, os
+
+def usage():
+    print "Usage: python run.py [notebooks]"
+    sys.exit(1)
+
+if __name__ == "__main__":
+    
+    # check for usage
+    if len(sys.argv) > 2:
+        usage()
+
+    # run notebooks, the default if no action specified
+    if len(sys.argv) == 1 or sys.argv[1] == 'notebooks':
+        cmd = 'ipython notebook --pylab=inline notebooks'
+        os.system(cmd)
+    else:
+        usage()
