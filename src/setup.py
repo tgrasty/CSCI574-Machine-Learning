a7ca46c5f53efc6ae1df01707cd1d9a24bc69842
Derek Harter <derek@harter.pro>
2020-07-06 20:27:32 -0500

    Add setup file to install ml_python_class package.

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Standard setup.py file to install the ml_python_class module for general use, without having to 
+setup PYTHON_PATH in notebooks.
+"""
+from setuptools import setup, find_packages
+
+setup(name="ml_python_class",
+      version="0.1",
+      description="Common functions and classes for the ml_python_class notebooks",
+      author="Derek Harter",
+      author_email="derek.harter@tamuc.edu")
